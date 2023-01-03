a460afbaa078cc5c0e911253ad992ae5985892e3
BetoCochas <oscargio86@hotmail.com>
2023-01-02 22:40:21 -0500

    'exercises'

---
+++ ejercicios-bucles/index.js
@@ -0,0 +1,11 @@
+/* Ejercicios
+1. Realizar un programa que permita el ingreso de un numero y muestre su tabla de
+multiplicar (Los primeros 10 multiplos). */
+let numero = [1,2,3,4,5,6,7,8,9,10]
+function multiplos(numero){
+
+ for(let i=1;i<=10;i++ ){
+    return( numero*i)
+}
+} 
+multiplos(5)
